0e7800bc5d4cbe13bb599deb161491e61916624e
Victor Martins <vitorchiesa74@gmail.com>
2022-09-13 15:13:33 -0300

    Including Bootstrap, CSS and JS

--- app/conf.php
+++ app/conf.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * configuration file
+
+ *  __FILE__ - Magic constant. Returns the full path and filename
+
+ *  dirname - Returns the path of the parent directory
+
+ *  define and const - Defines a constant. Constants cannot be changed once declared
+ */
+
 define('APP', dirname(__FILE__));
-define('URL', 'http://localhost/mvc-php-course/');
+define('URL', 'http://localhost/mvc-php-course');
 define('APP_NAME', 'Blog Games');
--- public/index.php
+++ public/index.php
@@ -1,23 +1,39 @@
 <?php
-    include_once './../app/conf.php';
-    include_once './../app/Lib/Routes.php';
-    include_once './../app/Lib/Controller.php';
+include_once './../app/conf.php';
+include_once './../app/Lib/Routes.php';
+include_once './../app/Lib/Controller.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> <?= APP_NAME ?> </title>
+
+    <!-- BOOTSRAP CDN -->
+    <!-- CSS ONLY -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="<?= URL ?>/public/css/style.css" rel="stylesheet">
+
 </head>
+
 <body>
 
     <?php
-        $routes = new Routes();
-        //$routes -> url();
+    $routes = new Routes();
     ?>
-    
+
+    <!-- BOOTSRAP CDN -->
+    <!-- SCRIPT ONLY -->
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"></script>
+    <script src="<?= URL ?>/public/js/script.js"></script>
 </body>
+
 </html>
