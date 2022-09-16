0b905a87307ffa9bb7da0ba12c34f9902b01facf
Victor Martins <vitorchiesa74@gmail.com>
2022-09-16 15:25:12 -0300

    Create user form

--- app/Views/users/register.php
+++ app/Views/users/register.php
@@ -1,1 +1,36 @@
-REGISTER VIEW
+<div class="col-xl-4 col-md-6 mx-auto p-5">
+    <div class="card">
+        <div class="card-body">
+            <h2>Register</h2>
+            <small>Fill in the form below to register</small>
+            <form name="resgiter" method="POST" action="">
+                <div class="form-group">
+                    <label for="name">Name: <sup class="text-danger">*</sup> </label>
+                    <input type="text" name="name" id="name" class="form-control" require>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email: <sup class="text-danger">*</sup> </label>
+                    <input type="email" name="email" id="email" class="form-control" require>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password: <sup class="text-danger">*</sup> </label>
+                    <input type="password" name="password" id="password" class="form-control" require>
+                </div>
+                <div class="form-group">
+                    <label for="password_confirm">Confirm the Password: <sup class="text-danger">*</sup> </label>
+                    <input type="password" name="password_confirm" id="password_confirm" class="form-control" require>
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <input type="submit" value="Register" class="btn btn-info btn-block">
+                    </div>
+                    <div class="col">
+                        <a href="">Do you have an account? log in</a>
+                    </div>
+                </div>
+
+            </form>
+        </div>
+    </div>
+</div>
