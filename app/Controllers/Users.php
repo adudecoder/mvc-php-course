bf085595c4f40e7f6cc5b62f6971540dddd0a976
Victor Martins <vitorchiesa74@gmail.com>
2022-09-16 20:34:23 -0300

    User registration form validation and logic Test

--- app/Controllers/Users.php
+++ app/Controllers/Users.php
@@ -16,30 +16,31 @@ class Users extends Controller
                 'password_confirm' => trim($form['password_confirm']),
             ];
 
-            if (empty($form['name'])) {
-                $dados['error_name'] = 'Fill in the name field';
-            }
+            if (in_array('', $form)) {
 
-            if (empty($form['email'])) {
-                $dados['error_email'] = 'Fill in the email field';
-            }
-
-            if (empty($form['password'])) {
-                $dados['error_password'] = 'Fill in the password field';
-            } else if (strlen($form['password']) < 6) {
-                $dados['error_password'] = 'Password must be at least 6 characters long';
-            }
-
-            if (empty($form['password_confirm'])) {
-                $dados['error_password_confirm'] = 'Confirm password';
-            } else {
-                if ($form['password'] != $form['password_confirm']) {
-                    $dados['error_password_confirm'] = 'The passwords are different';
+                if (empty($form['name'])) {
+                    $dados['error_name'] = 'Fill in the name field';
                 }
-            }
 
-            if (!in_array('', $form)) {
-                echo 'You can register';
+                if (empty($form['email'])) {
+                    $dados['error_email'] = 'Fill in the email field';
+                }
+
+                if (empty($form['password'])) {
+                    $dados['error_password'] = 'Fill in the password field';
+                }
+
+                if (empty($form['password_confirm'])) {
+                    $dados['error_password_confirm'] = 'Confirm password';
+                }
+            } else {
+                if (strlen($form['password']) < 6) {
+                    $dados['error_password'] = 'Password must be at least 6 characters long';
+                } else if ($form['password'] != $form['password_confirm']) {
+                    $dados['error_password_confirm'] = 'The passwords are different';
+                } else {
+                    echo 'You can register<hr>';
+                }
             }
 
             var_dump($form);
